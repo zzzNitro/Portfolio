265373f9fab209e4d47464c2f8dd37fa45ce4b77
Nitro <pablo.benito@rocketmail.com>
2022-06-14 06:03:22 -0400

    Metatags test

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -34,6 +34,12 @@ export default class MyDocument extends Document {
           <title>Pablo's Dexforge</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <meta name="description" content="Who is Pablo? Find out here!" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Pablo's Dexforge" />
+          <meta property="og:description" content="Who is Pablo? Find out here!" />
+          <meta property="og:url" content="https://pbreactprtfl.herokuapp.com/" />
+          <link rel="canonical" href="https://pbreactprtfl.herokuapp.com/" />
+          <meta property="og:image" content="./images/portfolio.png" />
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
