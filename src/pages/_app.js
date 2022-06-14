521cf0ad1a398f5cea720cfb54a7886e5420a28b
Nitro <pablo.benito@rocketmail.com>
2022-06-14 05:53:56 -0400

    added metatags for better sharing experience

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,10 @@
-import Head from 'next/head'
+//import Head from 'next/head'
 import Theme from '../styles/theme';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
-        <Head>
-          <title>Pablo's Dexforge</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
         <Component {...pageProps} />
       </Theme>
     </>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,6 +31,9 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
+          <title>Pablo's Dexforge</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="description" content="Who is Pablo? Find out here!" />
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
